18b439001aeb1b16b5510b341c58aabbaf1662e6
ricardomagallanes <ricardo.magallanes@tecnomag.com.ar>
2026-07-31 16:01:11 -0300

    Actualizar URL de ThingsBoard para Cooperativa B a eglig-186-122-131-134.free.pinggy.net

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ const thingsboardDashboardId = 'a40156d0-8c8b-11f1-8d8a-a962a2e26a4f';
 // URLs for Telemetry dashboards
 const telemetryUrls = {
   'coop-a': 'https://shenmu.usriot.com/share?s=fcc9ub9b1z&a=aHR0cHM6Ly9zaGVubXUudXNyaW90LmNvbS9zaGFyZQ==&l=en',
-  'coop-b': 'https://chastity-video-proactive.ngrok-free.dev/dashboard/a40156d0-8c8b-11f1-8d8a-a962a2e26a4f',
+  'coop-b': 'https://eglig-186-122-131-134.free.pinggy.net/dashboard/a40156d0-8c8b-11f1-8d8a-a962a2e26a4f',
   'coop-c': 'https://liberalistic-grinningly-caylee.ngrok-free.dev/nodered/ui/#!/0?socketid=XYKQyYr-wwbjIbqvAAAJ',
   'coop-d': 'https://f0a509af.us2a.app.preset.io/superset/embedded/0104b04a-5f7d-4f0c-b533-8df57fea43ee?standalone=true'
 };
